a33b6c614574b7d8c3eb94e46e334c2dc3977a40
harinnn <harinnnkim@gmail.com>
2023-08-23 22:27:34 +0900

    [EDIT] main 페이지 분리

--- hbd_kj_2023/app/page.tsx
+++ hbd_kj_2023/app/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 // Styles
-import { TextCakeContainer } from "./styles";
+import { HbdContainer } from "@/app/styles";
+
+// Components
+import Main from "../pages/main";
+import Photos from "../pages/photos";
 
 export default function Home() {
   return (
-    <TextCakeContainer>
-      <div className={"text-cake__candles"}>
-        <img src="/assets/img/ico-candle-three.png" alt="" />
-        <img src="/assets/img/ico-candle-zero.png" alt="" />
-      </div>
-      <div className={"text-cake__first-line"}>HAPPY</div>
-      <div className={"text-cake__middle-line"}>BIRTHDAY</div>
-      <div className={"text-cake__last-line"}>KYUJIN</div>
-    </TextCakeContainer>
+    <HbdContainer>
+      <Main />
+      <Photos />
+    </HbdContainer>
   );
 }
---
+++ hbd_kj_2023/pages/main/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+// Styles
+import { TextCakeContainer } from "./styles";
+
+export default function Index() {
+  return (
+    <TextCakeContainer>
+      <div className={"text-cake__candles"}>
+        <img src="/assets/img/ico-candle-three.png" alt="ico-candle-three" />
+        <img src="/assets/img/ico-candle-zero.png" alt="ico-candle-zero" />
+      </div>
+      <div className={"text-cake__first-line"}>HAPPY</div>
+      <div className={"text-cake__middle-line"}>BIRTHDAY</div>
+      <div className={"text-cake__last-line"}>KYUJIN</div>
+    </TextCakeContainer>
+  );
+}
